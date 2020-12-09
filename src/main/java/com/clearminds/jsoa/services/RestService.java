d5ceac9bd8d43cecc604462e4005fbdad02e084d
JuanTrop <jortiza2@unicartagena.edu.co>
2020-12-09 17:07:37 -0500

    Configurado con tomcat

--- src/main/java/com/clearminds/jsoa/services/RestService.java
+++ src/main/java/com/clearminds/jsoa/services/RestService.java
@@ -1,5 +1,18 @@
 package com.clearminds.jsoa.services;
 
+import com.clearminds.jsoa.dtos.Estudiante;
+import com.clearminds.jsoa.excepciones.BDDException;
+import com.clearminds.jsoa.servicios.ServicioEstudiante;
+
 public class RestService {
 
+public void insertarEstudiante(Estudiante estudiante){
+	ServicioEstudiante servEstudiante = new ServicioEstudiante();
+	try {
+		servEstudiante.insertarEstudiante(estudiante);
+	} catch (BDDException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+}
 }
