9fe527c3f36f96aeb2a5e3a085a00165f5d5e582
Juan Sebastian <hammerjuan_@hotmail.com>
2020-12-10 02:02:39 -0500

    Cambios hechos al restProject

--- src/main/java/com/clearminds/jsoa/services/RestService.java
+++ src/main/java/com/clearminds/jsoa/services/RestService.java
@@ -1,11 +1,21 @@
 package com.clearminds.jsoa.services;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 import com.clearminds.jsoa.dtos.Estudiante;
 import com.clearminds.jsoa.excepciones.BDDException;
 import com.clearminds.jsoa.servicios.ServicioEstudiante;
 
+@Path("/estudiantes/")
 public class RestService {
 
+@POST
+@Path("/insertar")
+@Consumes({MediaType.APPLICATION_JSON})
 public void insertarEstudiante(Estudiante estudiante){
 	ServicioEstudiante servEstudiante = new ServicioEstudiante();
 	try {
